f7375629c9ed592a77a6ce23ea265c25bda57f74
Brian Cloutier <bmcloutier@gmail.com>
2024-07-10 12:14:51 -0500

    Updated products controller to use view templates

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,11 +1,11 @@
 class ProductsController < ApplicationController
   def all_products_method
-    products = Product.all
-    render json: { products: products }
+    @products = Product.all
+    render template: "products/index"
   end
 
   def single_product_method
-    product = Product.first
-    render json: { name: product["name"], price: product["price"], image_url: product["image_url"], description: product["description"] }
+    @product = Product.first
+    render template: "products/show"
   end
 end

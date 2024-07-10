ecd2b3bc4698171d8e993b0ce27825a617a0947d
Brian Cloutier <bmcloutier@gmail.com>
2024-07-10 11:53:41 -0500

    Added products to database

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,12 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+product = Product.create(name: "Widget", price: 5, image_url: "images/widget.jpg", description: "The world's best thingy!")
+product.save
+
+product = Product.create(name: "Desk", price: 60, image_url: "images/desk.jpg", description: "A place to set your favorite widget!")
+product.save
+
+product = Product.create(name: "Chair", price: 35, image_url: "images/chair.jpg", description: "Sit at your desk in comfort!")
+product.save

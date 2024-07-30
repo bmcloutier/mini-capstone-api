6db2bf5297711ea16920293e180a85cb230490d0
Brian Cloutier <bmcloutier@gmail.com>
2024-07-30 13:23:34 -0500

    Using authentication and current_user to limit access

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,23 +1,35 @@
 class OrdersController < ApplicationController
-  def create()
-    @order = Order.create(
-      user_id: params[:user_id],
-      product_id: params[:product_id],
-      quantity: params[:quantity],
-      subtotal: params[:subtotal],
-      tax: params[:tax],
-      total: params[:total],
-    )
-    render json: { message: "created successfully" }
+  def create
+    if current_user != nil
+      @order = Order.create(
+        user_id: current_user.id,
+        product_id: params[:product_id],
+        quantity: params[:quantity],
+        subtotal: params[:subtotal],
+        tax: params[:tax],
+        total: params[:total],
+      )
+      render json: { message: "created successfully" }
+    else
+      render json: { message: "You can't create orders if not logged in!" }
+    end
   end
 
   def index
-    @orders = Order.all
-    render :index
+    if current_user != nil
+      @orders = Order.where(user_id: current_user.id)
+      render :index
+    else
+      render json: { message: "You must be logged in to see orders!" }
+    end
   end
 
   def show
     @order = Order.find_by(id: params[:id])
-    render :show
+    if current_user != nil && @order.user_id == current_user.id
+      render :show
+    else
+      render json: { message: "You can't view orders that aren't yours!" }
+    end
   end
 end

ebcce3955e603153f64aec0c20818541a148d882
Brian Cloutier <bmcloutier@gmail.com>
2024-07-31 11:28:45 -0500

    Added authorization to orders controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,39 +1,31 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user
+
   def create
-    if current_user != nil
-      @product = Product.find_by(id: params[:product_id])
-      subtotal = @product.price * params[:quantity].to_f
-      tax = subtotal * 0.085
-      total = subtotal + tax
-      @order = Order.create(
-        user_id: current_user.id,
-        product_id: params[:product_id],
-        quantity: params[:quantity],
-        subtotal: subtotal,
-        tax: tax,
-        total: total,
-      )
-      render :show
-    else
-      render json: { message: "You can't create orders if not logged in!" }
-    end
+    @product = Product.find_by(id: params[:product_id])
+    subtotal = @product.price * params[:quantity].to_f
+    tax = subtotal * 0.085
+    total = subtotal + tax
+    @order = Order.create(
+      user_id: current_user.id,
+      product_id: params[:product_id],
+      quantity: params[:quantity],
+      subtotal: subtotal,
+      tax: tax,
+      total: total,
+    )
+    render :show
   end
 
   def index
-    if current_user != nil
-      @orders = Order.where(user_id: current_user.id)
-      render :index
-    else
-      render json: { message: "You must be logged in to see orders!" }
-    end
+    @orders = Order.where(user_id: current_user.id)
+    render :index
   end
 
   def show
     @order = Order.find_by(id: params[:id])
-    if current_user != nil && @order.user_id == current_user.id
+    if @order.user_id == current_user.id
       render :show
-    else
-      render json: { message: "You can't view orders that aren't yours!" }
     end
   end
 end
